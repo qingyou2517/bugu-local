567906765a2079cae4e997922f840f40a875623b
tiga <2517789608@qq.com>
2022-08-27 21:44:14 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -24,15 +25,13 @@ const routes: Array<RouteConfig> = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    // 以上都没能匹配，则404
+    path: '*',
+    component: NotFound
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+
 ];
 
 const router = new VueRouter({

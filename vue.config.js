172deaed646b79a827ee5719441f6ff919697bc2
tiga <2517789608@qq.com>
2022-08-29 11:35:01 +0800

    尝试用svgo-loader删除svg可能自带的fill属性

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // 获取 icons 所在目录 => dir
     const dir = path.resolve(__dirname, 'src/assets/icons')
 
-    // 配置 svg-sprite-loader
+    // 配置 svg-sprite-loader 和 svgo-loader
     // Vue 把 webpack 的API封装并以 config 向外暴露给我们使用
     config.module
       .rule('svg-sprite')
@@ -15,6 +15,9 @@ module.exports = {
       .include.add(dir).end() // 只在 icons 目录里匹配.svg
       .use('svg-sprite-loader').loader('svg-sprite-loader')
       .options({extract: false}).end()    // 不解析出文件
+      .use('svgo-loader').loader('svgo-loader')
+      // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+      .end()    // 如果s  vg自带fill属性，删除svg里的fill
 
     // 配置 plugin 插件
     config.plugin('svg-sprite')

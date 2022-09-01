98e9c89ba946825aa3d5946136455503adda23d0
tiga <2517789608@qq.com>
2022-09-01 22:58:52 +0800

    完成 Notes 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,6 +13,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad, Layout}
@@ -20,14 +21,14 @@ export default {
 </script>
 
 <style lang="scss">
-  .layout-content{
-    display: flex;
-    flex-direction: column-reverse;
-  }
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
 </style>
 
 <style lang="scss" scoped>
-  @import "~@/assets/styles/helper.scss";
+@import "~@/assets/styles/helper.scss";
 </style>
 
 

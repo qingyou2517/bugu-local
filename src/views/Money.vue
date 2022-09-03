8d70d9a038e3fdf7d52e507d8e71a13599b9141c
tiga <2517789608@qq.com>
2022-09-03 17:04:12 +0800

    把record的复制保存至recordList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,8 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
-    <NumberPad @update:value="onUpdateAmount"/>
-    <Types :value.sync="record.type" />
+    {{ recordList }}
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags"
           @update:value="onUpdateTags"/>
@@ -15,13 +15,13 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 
-type Record={
-  tags:string[],
-  notes:string,
-  type:string,
-  amount:string
+type Record = {
+  tags: string[],
+  notes: string,
+  type: string,
+  amount: string
 }
 
 @Component({
@@ -29,20 +29,36 @@ type Record={
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  record:Record={
-    tags:[],
-    notes:'',
-    type:'',
-    amount:'0'
+  // Record类型的数组，每一项都是一个Record类型的数据
+  recordList: Record[]=[];
+  record: Record = {
+    tags: [],
+    notes: '',
+    type: '',
+    amount: '0'
+  };
+
+  onUpdateTags(tags: string[]) {
+    this.record.tags = tags;
   }
-  onUpdateTags(tags:string[]){
-    this.record.tags=tags
+
+  onUpdateNotes(notes: string) {
+    this.record.notes = notes;
   }
-  onUpdateNotes(notes:string){
-    this.record.notes=notes
+
+  onUpdateAmount(value: string) {
+    this.record.amount = value;
   }
-  onUpdateAmount(value:string){
-    this.record.amount=value
+
+  saveRecord() {
+    const record2 = JSON.parse(JSON.stringify(this.record));
+    this.recordList.push(record2);
+    console.log(this.recordList);
+  }
+
+  @Watch('recordList')
+  onRecordListChanged() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.record));
   }
 }
 </script>

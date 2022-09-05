d6326287e74f36c2e6fbac961431ffa17c63e30a
tiga <2517789608@qq.com>
2022-09-05 17:40:07 +0800

    展示tag.name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,12 +1,12 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left" />
+      <Icon class="leftIcon" name="left"/>
       <span class="title">编辑标签</span>
-      <span class="rightIcon" />
+      <span class="rightIcon"/>
     </div>
     <div class="Notes-wrapper">
-      <Notes field-name="标签名" placeholder="请输入标签名"/>
+      <Notes :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="Button-wrapper">
       <Button>删除标签</Button>
@@ -20,17 +20,20 @@ import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 import Notes from '@/components/Money/Notes.vue';
 import Button from '@/components/Button.vue';
+
 @Component({
   components: {Button, Notes}
 })
 export default class EditLabel extends Vue {
+  tag?: { id: string, name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(item => item.id === id)[0];
     if (tag) {
-      console.log(tag);
+      this.tag=tag;
     } else {
       this.$router.replace('/404');
     }
@@ -39,34 +42,39 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang='scss' scoped>
-  .navBar{
-    text-align: center;
-    font-size: 16px;
-    padding: 12px 16px;
-    background: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    >.title{
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    }
-    >.leftIcon{
-      width: 24px;
-      height: 24px;
-    }
-    >.rightIcon{
-      width: 24px;
-      height: 24px;
-    }
+  > .title {
+
   }
-  .Notes-wrapper{
-    background: #fff;
-    margin-top: 8px;
+
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
   }
-  .Button-wrapper{
-    text-align: center;
-    padding: 16px;
-    margin-top: 44-16px;
+
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
   }
+}
+
+.Notes-wrapper {
+  background: #fff;
+  margin-top: 8px;
+}
+
+.Button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 
 </style>

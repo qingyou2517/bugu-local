a8610d105949937ddb2e564be7df8d24aad24dee
tiga <2517789608@qq.com>
2022-09-08 21:11:14 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,14 +19,16 @@ import {Component} from 'vue-property-decorator';
 @Component({
   computed: {
     tagList() {
-      // TODO
-      // return this.$store.fetchTags();
-      return [];
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -44,8 +46,7 @@ export default class Tags extends Vue {
       window.alert('标签名不能为空！');
       return;
     }
-    //TODO
-    // store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>

5cd78a324b89bd9997c492ed09df186447f069bb
tiga <2517789608@qq.com>
2022-09-11 11:12:32 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,7 +15,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
@@ -23,7 +22,7 @@ import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tabs, Tags, Notes, Types, NumberPad},
+  components: {Tabs, Tags, Notes, NumberPad},
 })
 export default class Money extends Vue {
   get recordList() {

f2b7aa7cf08d26baa8c96bd91b45144c37292248
tiga <2517789608@qq.com>
2022-09-12 17:17:18 +0800

    记账金额小数点后只能两位

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -44,7 +44,12 @@ export default class NumberPad extends Vue {
       }
       return;
     }
-    if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+    const index = this.output.indexOf('.')
+    if (index >= 0) {
+      const index2 = this.output.indexOf('.') + 3;
+      if (this.output.length >= index2) {return;}
+      if ( input === '.') {return;}
+    }
     this.output += input;
   }
 
@@ -61,7 +66,7 @@ export default class NumberPad extends Vue {
     const number = parseFloat(this.output);
     this.$emit('update:value', number);
     this.$emit('submit', number);
-    this.output='0'
+    this.output = '0';
   }
 }
 </script>

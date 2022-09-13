7db18f51c18ce1dc6e4ba20f7dfac760f5a8bac6
tiga <2517789608@qq.com>
2022-09-13 16:27:32 +0800

    watch自动更新options

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -4,20 +4,27 @@
 
 <script lang='ts'>
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 import {EChartsOption} from 'echarts';
 import * as echarts from 'echarts';
 
 @Component
 export default class Chart extends Vue {
   @Prop() options?: EChartsOption;
+  chart?:echarts.ECharts
 
   mounted() {
     if (this.options === undefined) {
       return console.error('options 为空');
     }
-    const chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
-    chart.setOption(this.options);
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+    this.chart .setOption(this.options);
+  }
+  //不能只在 mounted 时 setOption，否则切换到“支出”，chart不变；
+  //应该只要options变化，就重新渲染chart
+  @Watch('options')
+  onOptionsChange(newValue:EChartsOption){
+    this.chart!.setOption(newValue)
   }
 }
 </script>

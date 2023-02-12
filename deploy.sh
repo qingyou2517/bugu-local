570915b91c74451032f29187b41ff36e6183f758
青游 <2517789608@qq.com>
2023-02-12 10:03:13 +0800

    切换到https连接方式

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:qingyou2517/Morney-website.git master:gh-pages
+git push -f https://github.com/qingyou2517/Morney-website.git master:gh-pages
 
 cd -
